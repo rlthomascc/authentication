3313c2e8304280508dbccf1ffec7a055fb23ef3b
Randy <rlthomasccv@gmail.com>
2019-02-08 20:10:15 -0800

    added util functions

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
+import { getFromStorage, setInStorage } from '../../utils/storage';
+import $ from 'jquery';
 import { Route, HashRouter } from 'react-router-dom';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoading: true,
+      token: '',
+      signUpError: '',
+      signInError: '',
+    }
+  }
+
+  componentDidMount() {
+    const token = getFromStorage('the_main_app');
+    if (token) {
+      // Verify the token
+      fetch(`/verify?token=${token}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.success) {
+          this.setState({
+            token: token,
+            isLoading: false,
+          })
+        } else {
+          this.setState({
+            isLoading: false,
+          })
+        }
+      });
+    } else {
+      this.setState({
+        isLoading: false,
+      });
+    }
   }
 
   homePage() {
-    return (
-      <div>
-        <h1>YOU MADE IT!</h1>
-      </div>
-    );
+    const {isLoading} = this.state
+    if (isLoading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+        return (
+        <div id="logout">
+          <button type="submit" className="btn btn-danger btn-lg">Log Out</button>
+        </div>
+        );
   }
 
   render() {
